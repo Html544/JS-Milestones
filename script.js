66df6e037ee9d01744307efbbe2b489726a7b740
EmanRiaz <emanriaz504@gmail.com>
2024-08-24 21:28:33 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+/*Question No 1 */
+alert("Welcome to our project!");
+/*Question No 2*/
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+/*Question No 3*/
+ alert("Script in Head");
+/* Question No 4*/
+alert("Script Before HTML");
+/*Question No 5*/
+alert("Script Inside HTML");
+/*Question No 6*/
+alert("Script After HTML");
+/*Question No 7*/
+/*window.onload = function() {
+    alert("Welcome to our website!");*/
+/*Queation No 8
+ alert("Welcome to JS Land...\nHappy Coding!");
+/* Question No 9*/
+/* alert("Error! Please enter a valid password.");*/
+ /*Question No 10*/
+/* console.log("Hello.. I can run JS through my web browser's console");*/
+ 
